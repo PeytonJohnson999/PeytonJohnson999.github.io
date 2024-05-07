189203afb9a8a1e5f1eb4a7ee4b8fdcbd1d15ad6
PeytonJohnson999 <Johnso630@stcharles.k12.la.us>
2024-05-07 15:38:32 +0000

    pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -14,9 +14,13 @@ function runProgram(){
   const BOARD_HEIGHT = $("#board").height();
   const PADDLE_WIDTH = $("#paddle1").width();
   const PADDLE_HEIGHT = $("#paddle2").height();
-  const X_OFFSET = 10;
+  const XY_OFFSET = 10;
   const NOTHING = 0;  //NO MAGIC NUMBERS
   const PADDLE_SPEED = 5;
+  const BALL_WIDTH = $("#ball").width();
+  const BALL_HEIGHT = $("#ball").height();
+
+
   const KEY_CODE= {
     W : 87,
     A : 65,
@@ -31,23 +35,46 @@ function runProgram(){
   
   // Game Item Objects
   const paddle1 = {
-    x : X_OFFSET,
-    y : 0,
+    x : XY_OFFSET,
+    y : (BOARD_HEIGHT / 2) - (PADDLE_HEIGHT / 2),
     velocityY : 0,
     id : "#paddle1",
   }
 
   const paddle2 = {
-    x : BOARD_WIDTH - (PADDLE_WIDTH + X_OFFSET),  
-    y : 0,
+    x : BOARD_WIDTH - (PADDLE_WIDTH + XY_OFFSET),  
+    y : (BOARD_HEIGHT / 2) - (PADDLE_HEIGHT/ 2),
     velocityY : 0,
     id : "#paddle2",
+  }
+
+  const ball = {
+    x : (BOARD_WIDTH / 2) - (BALL_WIDTH / 2),
+    y : (BOARD_HEIGHT / 2) - (BALL_HEIGHT / 2),
+    velocityX : 5,
+    velocityY : 5,
+    mvmntSpeed : 5,
+    id : "#ball"
+  }
+
+  // Player Objects
+  const player1 = {
+    score : 0,
+    scoreId : "#plr1Score",
+    paddle : paddle1,
+  }
+
+  const player2 = {
+    score : 0,
+    scoreId : "#plr2Score",
+    paddle : paddle1,
   }
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);
   $(document).on('keyup', handleKeyUp);  
+
                            // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -59,10 +86,14 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
+    // moveBall(ball);
     updatePosition(paddle1)
     updatePosition(paddle2)
+    updateBall(ball)
     checkBorderCollision(paddle1);
     checkBorderCollision(paddle2)
+    checkBallWallCollision(ball)
+    checkBallPaddleCollision(ball)
   }
   
 
@@ -90,6 +121,12 @@ function runProgram(){
     }
   }
 
+  ///////////
+  ///////////
+  // PLAYER 2
+  ///////////
+  ///////////
+
   function handleKeyUp(event){
     //TODO: FLUID MOVEMENT AND PLAYER 2 MOVEMENT
     if ((event.which === KEY_CODE.W && paddle1.velocityY <= 0) || (event.which === KEY_CODE.S && paddle1.velocityY >= 0)){
@@ -99,17 +136,24 @@ function runProgram(){
       paddle2.velocityY = 0;
     }
   }
+
+  
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
   
   function endGame() {
-    // stop the interval timer
-    clearInterval(interval);
-
-    // turn off event handlers
-    $(document).off();
+    
+    if (player1.score === 5 || player2.score === 5){
+      // stop the interval timer
+      // turn off event handlers
+      clearInterval(interval);
+      $(document).off();
+    }else {
+      restartGame();
+    }
+    
   }
 
   function updatePosition(paddle){
@@ -118,15 +162,104 @@ function runProgram(){
     $(paddle.id).css("top", paddle.y);
   }
 
-  function canMove(paddle, direction){
-    
+  function updateBall(ball){
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+    $(ball.id).css("left", ball.x);
+    $(ball.id).css("top", ball.y);
   }
 
+  //Checks border collision for the paddles
   function checkBorderCollision(paddle){
     //TODO: FIX
     if ((paddle.y >= BOARD_HEIGHT - PADDLE_HEIGHT && !(paddle.velocityY <= 0)) || (paddle.y <= 0 && !(paddle.velocityY >= 0))) {
       paddle.velocityY = 0;
     }
   }
+
+  //Checks border collision for the ball
+  function checkBallWallCollision(ball) {
+
+    // if ball hits right wall
+    if (ball.x > BOARD_WIDTH - BALL_WIDTH && !(ball.velocityX < 0)){
+      player1.score++
+      updateScore()
+      console.log(player1.score)
+      endGame()
+    }
+
+    // if ball hits left wall
+    if (ball.x < XY_OFFSET && !(ball.velocityX > 0)){
+      player2.score++
+      updateScore()
+      console.log(player2.score)
+      endGame()
+    }
+
+    // if ball hits the lower wall
+    if (ball.y > BOARD_HEIGHT - BALL_HEIGHT && !(ball.velocityY < 0) ) {
+      ball.velocityY *= -1
+    }
+
+    // if the ball hits the upper wall
+    if (ball.y < XY_OFFSET && !(ball.velocityY > 0)){
+      ball.velocityY *= -1
+    }
+  }
+
+  function checkBallPaddleCollision(ball){
+    
+    // If the ball collides with the right side of paddle1
+    if ((ball.x >= paddle1.x && ball.x <= paddle1.x + PADDLE_WIDTH) && 
+    (ball.y <= paddle1.y + PADDLE_HEIGHT && ball.y >= paddle1.y)){
+      ball.velocityX++
+      ball.velocityX *= -1
+    }
+
+    // if the ball collides with the upper side of paddle1
+    // if ((ball.x >= paddle1.x && ball.x <= paddle1.x + PADDLE_WIDTH) &&
+    // (ball.y + BALL_HEIGHT >= paddle1.y - ball.mvmntSpeed && ball.y + BALL_HEIGHT <= paddle1.y + ball.mvmntSpeed)){
+    //   //Sucks bc of stupid fps oriented design
+    //   ball.velocityY *= -1;
+    // }
+
+    // if the ball collides with the lower side of paddle1
+    // if ((ball.x >= paddle1.x && ball.x <= paddle1.x + PADDLE_WIDTH) &&
+    // (ball.y + BALL_HEIGHT >= (paddle1.y + PADDLE_HEIGHT) - ball.mvmntSpeed && ball.y + BALL_HEIGHT <= (paddle1.y + PADDLE_HEIGHT) + ball.mvmntSpeed)){
+    //   //Sucks bc of stupid fps oriented design
+    //   ball.velocityY *= -1;
+    // }
+
+    // if the ball collides with the left side of paddle2
+    if ((ball.x <= paddle2.x && ball.x >= paddle2.x - PADDLE_WIDTH) && 
+    (ball.y <= paddle2.y + PADDLE_HEIGHT && ball.y >= paddle2.y)){
+      ball.velocityX++
+      ball.velocityX *= -1
+    }
+
+    
+  }
+
+  function restartGame(){
+    resetBall()
+    resetPaddle(paddle1)
+    resetPaddle(paddle2)
+  }
+
+  function resetBall(){
+    ball.x = (BOARD_WIDTH / 2) - (BALL_WIDTH / 2)
+    ball.y = (BOARD_HEIGHT / 2) - (BALL_HEIGHT / 2)
+    ball.velocityX = ball.mvmntSpeed
+    ball.velocityY = ball.mvmntSpeed
+  }
   
+  function resetPaddle(paddle){
+    paddle.y = (BOARD_HEIGHT / 2) - (PADDLE_HEIGHT/ 2)
+    paddle.velocityY = 0
+  }
+  
+  function updateScore(){
+    $(player1.scoreId).text("Player 1: " + player1.score)
+    $(player2.scoreId).text("Player 2: " + player2.score)
+  }
 }
